47231b750831455578f759895c15bd0f5b9ae382
Megzz02 <ahmedsaleh.ams02@gmail.com>
2023-11-19 22:26:29 +0800

    Q2 todo parts
    Completion of todo parts for q2

--- q2/q2.py
+++ q2/q2.py
@@ -44,21 +44,55 @@ def a_star_search(initial_position, goal_position, rubbish_positions):
     # Calculate f for the start state
     h_start = State.heuristic(start_state.position, rubbish_positions, goal_position)
     f_start = start_state.energy_used + h_start  # g(n) is 0 for start state
-    
+
     # Add start state to the open list
     open_list.append((f_start, start_state))
-    
+
     while open_list:
-        # TODO: Get the state with the lowest f from the open list
-        # TODO: If this state is the goal, reconstruct the path and return it
-        # TODO: Generate successor states and set their g, h, and f values
-        # TODO: For each successor
-        # - If successor is in the closed list and has a lower or equal f, skip
-        # - If successor is in the open list and has a lower or equal f, skip
-        # - Otherwise, add successor to the open list
-        
-        # TODO: Add the current state to the closed list
-        pass
+        # Get the state with the lowest f from the open list
+        current_state = min(open_list, key=lambda x: x[0])[1]
+        open_list = [item for item in open_list if item[1] != current_state]
+        closed_list.append(current_state)
+
+        # If this state is the goal, reconstruct the path and return it
+        if current_state.position == goal_position:
+            path = [current_state.position]
+            while current_state.position != initial_position:
+                current_state = min([state for state in closed_list if state.position == current_state.position],
+                                   key=lambda x: x.energy_used)
+                path.insert(0, current_state.position)
+            return path
+
+        # Generate successor states and set their g, h, and f values
+        successors = []
+        for neighbor in [(current_state.position[0] + 1, current_state.position[1]),
+                         (current_state.position[0] - 1, current_state.position[1]),
+                         (current_state.position[0], current_state.position[1] + 1),
+                         (current_state.position[0], current_state.position[1] - 1)]:
+            # Check if the neighbor is within the grid
+            if neighbor[0] >= -2 and neighbor[1] >= -2:
+                # Check if the neighbor is not blocked by rubbish
+                if neighbor not in rubbish_positions or neighbor in current_state.collected_rubbish:
+                    # Calculate the new state
+                    collected_rubbish = current_state.collected_rubbish.copy()
+                    if neighbor in rubbish_positions:
+                        collected_rubbish.append(rubbish_positions[neighbor])
+                    new_state = State(neighbor, collected_rubbish, current_state.energy_used + current_state.energy_to_move())
+                    # Calculate h and f for the new state
+                    h_new = State.heuristic(new_state.position, rubbish_positions, goal_position)
+                    f_new = new_state.energy_used + h_new
+
+                    successors.append((f_new, new_state))
+
+        for successor in successors:
+            # If successor is in the closed list and has a lower or equal f, skip
+            if any(successor[1].position == state.position and successor[0] >= state.energy_used + State.heuristic(state.position, rubbish_positions, goal_position) for state in closed_list):
+                continue
+            # If successor is in the open list and has a lower or equal f, skip
+            if any(successor[1].position == state[1].position and successor[0] >= state[0] for state in open_list):
+                continue
+            # Otherwise, add successor to the open list
+            open_list.append(successor)
 
     # If no solution found
     return None
